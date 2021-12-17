3bc4ad4061f14cff9325ea4693248fd04d12e074
SvetkaKo <skovakina@gmail.com>
2021-12-18 00:50:28 +0300

    now app doesn't push empty strings in the list

--- app.js
+++ app.js
@@ -1,157 +1,159 @@
 //jshint esversion:6
 
-const express = require("express")
-const bodyParser = require("body-parser")
-const mongoose = require('mongoose');
-const _ = require('lodash')
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const _ = require("lodash");
 
-const app = express()
+const app = express();
 
 // const items = [  "Buy Food",  "Cook Food",  "Eat Food"];
 // const workItems = []
 
 //mongoDB database
-mongoose.connect('mongodb://localhost:27017/todolistDB', {
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
 const itemsSchema = {
-  name: String
+  name: String,
 };
 
 const Item = mongoose.model("Item", itemsSchema);
 
 const item1 = new Item({
-  name: "Welcome!"
-})
+  name: "Welcome!",
+});
 
 const item2 = new Item({
-  name: "Hit the + button to add a new item"
-})
+  name: "Hit the + button to add a new item",
+});
 
 const item3 = new Item({
-  name: "Hit this to delete an item"
-})
+  name: "Hit this to delete an item",
+});
 
-const defaultItems = [item1, item2, item3]
+const defaultItems = [item1, item2, item3];
 
 const listSchema = {
   name: String,
-  items: [itemsSchema]
-}
+  items: [itemsSchema],
+};
 
-const List = mongoose.model("List", listSchema)
+const List = mongoose.model("List", listSchema);
 
+app.set("view engine", "ejs");
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.static("public"));
 
-app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
-app.use(express.static("public"))
-
-app.get("/", function(req, res) {
-
-
-  Item.find({}, function(err, foundItems) {
-
+app.get("/", function (req, res) {
+  Item.find({}, function (err, foundItems) {
     if (foundItems.length === 0) {
-      Item.insertMany(defaultItems, function(err) {
+      Item.insertMany(defaultItems, function (err) {
         if (err) {
           console.log(err);
         } else {
           console.log("Success!");
         }
-      })
-      res.redirect("/")
+      });
+      res.redirect("/");
     } else {
       res.render("list", {
         listTitle: "Today",
-        newListItems: foundItems
-      })
+        newListItems: foundItems,
+      });
     }
-  })
-})
+  });
+});
 
-app.post("/", function(req, res) {
+app.post("/", function (req, res) {
+  const itemName = req.body.newItem.trim();
+  const listName = req.body.list;
+  //check if string is empty, and if it's not => push to list
+  if (itemName != "") {
+    const item = new Item({
+      name: itemName,
+    });
 
-  const itemName = req.body.newItem
-  const listName = req.body.list
-  const item = new Item({
-    name: itemName
-  })
-
-  if (listName === "Today") {
-    item.save()
-    res.redirect("/")
+    if (listName === "Today") {
+      item.save();
+      res.redirect("/");
+    } else {
+      List.findOne({ name: listName }, function (err, foundList) {
+        foundList.items.push(item);
+        foundList.save();
+        res.redirect("/" + listName);
+      });
+    }
   } else {
-    List.findOne({name: listName}, function(err, foundList){
-      foundList.items.push(item)
-      foundList.save()
-      res.redirect("/" + listName)
-    })
+    console.log("empty input");
+    res.redirect("/");
   }
+});
 
-
-})
-
-app.post("/delete", function(req, res) {
-  const checkedItemId = req.body.checkbox
-  const listName = req.body.listName
+app.post("/delete", function (req, res) {
+  const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
   if (listName === "Today") {
-    Item.findByIdAndRemove(checkedItemId, function(err) {
+    Item.findByIdAndRemove(checkedItemId, function (err) {
       if (!err) {
         console.log("Successfully deleted checked item");
-        res.redirect("/")
-      };
-    })
+        res.redirect("/");
+      }
+    });
   } else {
-    List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}, function(err, foundList){
-      if (!err) {
-        res.redirect("/" + listName)}
-    })
+    List.findOneAndUpdate(
+      { name: listName },
+      { $pull: { items: { _id: checkedItemId } } },
+      function (err, foundList) {
+        if (!err) {
+          res.redirect("/" + listName);
+        }
+      }
+    );
   }
+});
 
+app.get("/:customListName", function (req, res) {
+  const customListName = _.capitalize(req.params.customListName);
 
-})
-
-app.get("/:customListName", function(req, res) {
-  const customListName = _.capitalize(req.params.customListName)
-
-
-  List.findOne({name: customListName}, function(err, foundList){
-    if (!err){
+  List.findOne({ name: customListName }, function (err, foundList) {
+    if (!err) {
       if (!foundList) {
         //Create a new list
         const list = new List({
           name: customListName,
-          items: defaultItems
-        })
+          items: defaultItems,
+        });
 
-        list.save()
+        list.save();
         res.redirect("/" + customListName);
       } else {
         //Show an existing list
         res.render("list", {
           listTitle: foundList.name,
-          newListItems: foundList.items
-        })
+          newListItems: foundList.items,
+        });
       }
     }
-  })
-})
+  });
+});
 
+app.post("/work", function (req, res) {
+  let item = req.body.newListItems;
+  workItems.push(item);
+  res.redirect("/work");
+});
 
-app.post("/work", function(req, res) {
-  let item = req.body.newListItems
-  workItems.push(item)
-  res.redirect("/work")
-})
+// app.get("/about", function (req, res) {
+//   res.render("about");
+// });
 
-app.get("/about", function(req, res) {
-  res.render("about")
-})
-
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server is running on port 3000");
-})
+});
